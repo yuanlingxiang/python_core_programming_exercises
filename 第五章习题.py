7ec567567f988fc22c3b105d759b4cfd4964ce42
yuanlingxiang <ylx330328@163.com>
2018-10-13 13:58:51 +0800

    第五章习题

---
+++ 第五章习题.py
@@ -0,0 +1,15 @@
+#!usr/bin/env/python
+# -*-coding:utf-8 -*-
+
+"""python核心编程第五章习题"""
+
+
+def func1():
+    """整型和长整型的区别：
+    现在没有明显的区别,显示显示为长整型只需要值后面加l
+    """
+    a = 1l
+    b = 1
+    return a, b
+
+
